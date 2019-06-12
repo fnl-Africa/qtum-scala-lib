6f61e2d19af6fb4d661669447d58fa1735fcadac
CodeFace <cnchenjun@qq.com>
2019-06-12 17:26:11 +0800

    fix test: calculate next work required

--- src/test/scala/org/qtumproject/qtum/reference/PowSpec.scala
+++ src/test/scala/org/qtumproject/qtum/reference/PowSpec.scala
@@ -5,7 +5,10 @@ import org.scalatest.FunSuite
 
 class PowSpec extends FunSuite {
   test("calculate next work required") {
-    val header = BlockHeader(version = 2, hashPreviousBlock = ByteVector32.Zeroes, hashMerkleRoot = ByteVector32.Zeroes, time = 0L, bits = 0L, nonce = 0L)
+    val header = BlockHeader(version = 2, hashPreviousBlock = ByteVector32.Zeroes,
+      hashMerkleRoot = ByteVector32.Zeroes, time = 0L, bits = 0L, nonce = 0L,
+      hashStateRoot = ByteVector32.Zeroes, hashUTXORoot = ByteVector32.Zeroes,
+      prev_stake_hash = ByteVector32.Zeroes, prev_stake_n = 0L, sizeVchSig = 0, vchSig = Array())
 
     assert(BlockHeader.calculateNextWorkRequired(header.copy(time = 1262152739, bits = 0x1d00ffff), 1261130161) === 0x1d00d86aL)
     assert(BlockHeader.calculateNextWorkRequired(header.copy(time = 1233061996, bits = 0x1d00ffff), 1231006505) === 0x1d00ffffL)
